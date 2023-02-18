cd84751cc1ca7f0a3acdb421b6839d5f25b1fc34
shreyahegde12 <84639200+shreyahegde12@users.noreply.github.com>
2023-02-18 21:39:54 +0530

    Create another.cpp

---
+++ another.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
+  cout << "This is another .cpp file\n";
+  cout << "I have successfully built and run\n";
+  return 0;
+}
